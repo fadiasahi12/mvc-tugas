c928b4831395a7dc8ecff8962f067defb61bde28
fadiasahi12 <113955448+fadiasahi12@users.noreply.github.com>
2022-11-17 14:15:54 +0700

    tugas 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php 
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Ilham Nawawi
+        Saya tinggal di daerah ciputat
+        olahraga yang saya sukai adalah
+        Bulutangkis dan berenang";
+    }
+    
+}
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <a href=‛http://www.RentalBuku.com‛>RentalBuku hello</a>
+        </footer>
+    </div>
+</body>
+</html>
